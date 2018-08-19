7296af46d5ffd7fa7cf3da41a31d40faa72e9fd3
ashish-greycube <mr.ashish.shah@gmail.com>
2018-08-19 16:58:24 +0530

    tortal pass 3

--- tortal_lms_integration/tortal_lms_integration/doctype/tortal_lms_system_settings/tortal_lms_system_settings.js
+++ tortal_lms_integration/tortal_lms_integration/doctype/tortal_lms_system_settings/tortal_lms_system_settings.js
@@ -9,7 +9,7 @@ frappe.ui.form.on('Tortal LMS System Settings', {
 
 	take_upload: function(frm) {
 		if (frm.doc.ftp_address && frm.doc.ftp_username && frm.doc.ftp_password && frm.doc.is_integration_active==1 ) {
-			frm.add_custom_button(__("Uploadp Now"), function(){
+			frm.add_custom_button(__("Manual Upload"), function(){
 				frm.dashboard.set_headline_alert("Tortal Upload Started!");
 				frappe.call({
 					method: "tortal_lms_integration.tortal_lms_integration.doctype.tortal_lms_system_settings.tortal_lms_system_settings.take_upload_to_tortal",
